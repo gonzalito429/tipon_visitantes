2de995e00f24adeaf0f77bc2bdb1e1f16695e9e9
gonzalito429 <germanchillihuani3@gmail.com>
2025-07-23 11:19:21 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,10 +1,29 @@
-<?php
-function conectarBD() {
-    $conn_string = "host=localhost port=5432 dbname=tipon user=postgres password=123456";
-    $conexion = pg_connect($conn_string);
+?php
+class Database {
+    // --- MODIFICA ESTOS DATOS CON LOS TUYOS ---
+    private $host = 'dpg-d20fioeuk2gs73c7q290-a';          // Host de PostgreSQL
+    private $db_name = 'entrada_tipon';     // Nombre de tu base de datos
+    private $username = 'entrada_tipon_user';       // Tu usuario de PostgreSQL
+    private  $password = 'i4Fv9qZLR9C3THiUGU8LLFNhUypiKR8y';  // Tu contraseña de PostgreSQL
+    private $port = '5432';               // Puerto (usualmente 5432)
+    // -----------------------------------------
 
-    if (!$conexion) {
-        die("Error al conectar a la base de datos PostgreSQL: " . pg_last_error());
+    private $conn;
+
+    public function connect() {
+        if ($this->conn) {
+            return $this->conn;
+        }
+
+        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+
+        try {
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die('Error de Conexión: ' . $e->getMessage());
+        }
+
+        return $this->conn;
     }
-    return $conexion;
 }
