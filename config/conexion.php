644622af6dfafb2cb42f0232bdf000ae62143986
gonzalito429 <germanchillihuani3@gmail.com>
2025-07-23 11:43:12 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,19 +1,14 @@
-?php
+<?php
 class Database {
-    // --- MODIFICA ESTOS DATOS CON LOS TUYOS ---
-    private $host = 'dpg-d20fioeuk2gs73c7q290-a';          // Host de PostgreSQL
-    private $db_name = 'entrada_tipon';     // Nombre de tu base de datos
-    private $username = 'entrada_tipon_user';       // Tu usuario de PostgreSQL
-    private  $password = 'i4Fv9qZLR9C3THiUGU8LLFNhUypiKR8y';  // Tu contraseña de PostgreSQL
-    private $port = '5432';               // Puerto (usualmente 5432)
-    // -----------------------------------------
-
+    private $host = 'dpg-d20fioeuk2gs73c7q290-a';
+    private $db_name = 'entrada_tipon';
+    private $username = 'entrada_tipon_user';
+    private $password = 'i4Fv9qZLR9C3THiUGU8LLFNhUypiKR8y';
+    private $port = '5432';
     private $conn;
 
-    public function connect() {
-        if ($this->conn) {
-            return $this->conn;
-        }
+    public function conectar() {
+        if ($this->conn) return $this->conn;
 
         $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
 
