66b1f34ae49f35d62ee54e119997404a56349f66
gonzalito429 <germanchillihuani3@gmail.com>
2025-07-25 08:26:59 -0500

    Update index.php

--- views/index.php
+++ views/index.php
@@ -34,7 +34,6 @@ if (!$consulta) {
     <a href="index.php">Inicio</a> |
     <a href="registrar.php"> Registrar Entrada</a> |
     <a href="salida.php"> Registrar Salida</a> |
-
     <a href="../controllers/logout.php"> Cerrar Sesión</a>
   </p>
 
